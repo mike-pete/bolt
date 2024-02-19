830b4415f5d71580162d0ae45c5cc79948ef415e
Mike Peterson <mike@lemonshell.com>
2024-02-19 09:33:46 -0700

    handle missing window

--- web/src/app/extension/_interactions/bi.ts
+++ web/src/app/extension/_interactions/bi.ts
@@ -1,22 +1,40 @@
-"use client"
-import bime from '@mike.pete/bime'
-import handleLocationChanged from './handleLocationChanged'
+"use client";
+import bime from "@mike.pete/bime";
+import handleLocationChanged from "./handleLocationChanged";
 
-const target = window.parent
+const bi = () => {
+	// Node doesn't know about window, so if window isn't available, just return a proxy that throws an error (instead of returning bime).
+	if (typeof window === "undefined") {
+    const handler: ProxyHandler<Record<string, () => void>> = {
+      get: () => {
+        return async () => {
+          throw new Error("window is not available in this context");
+        };
+      },
+    };
 
-const model = {
+    return new Proxy<Record<string, () => void>>(
+      {} as Record<string, () => void>,
+      handler,
+    );
+  }
+
+  const target = window.parent || window;
+
+  const model = {
     locationChanged: handleLocationChanged,
-}
+  };
 
-bime.listen('*', model)
+  bime.listen("*", model);
 
-type ContentScriptModel = {
-    getTextContent: (selector: string) => Promise<string | undefined>
-    hideIframe: () => void
-    showIframe: () => void
-    goToPage: (url: string) => void
-}
+  type ContentScriptModel = {
+    getTextContent: (selector: string) => Promise<string | undefined>;
+    hideIframe: () => void;
+    showIframe: () => void;
+    goToPage: (url: string) => void;
+  };
 
-const bi = bime.target<ContentScriptModel>(target, '*')
+  return bime.target<ContentScriptModel>(target, "*");
+};
 
-export default bi
+export default bi();
