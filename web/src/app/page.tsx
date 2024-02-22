f53b01a6b3c44c1275c8da3bea47fbd7fcf3bbd0
Mike Peterson <mike@lemonshell.com>
2024-02-21 22:56:38 -0700

    start improving home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import BuyWithStripe from "./_components/BuyWithStripe";
 import NavBar from "./_components/NavBar";
 
@@ -5,8 +6,22 @@ export default async function Home() {
   return (
     <div>
       <NavBar />
-      <div className="flex items-center">
-        <BuyWithStripe />
+      <div className="flex justify-center">
+        <div className="flex max-w-md flex-col p-8">
+          <section className="flex flex-col h-screen">
+            <div className="flex flex-col items-center h-4/6 justify-center">
+              <div className="flex items-center gap-4">
+                <Image src="/bolt.svg" alt="Bolt" width={40} height={40} />
+                <h1 className="text-7xl font-semibold">Bolt</h1>
+              </div>
+              <h3 className="text-lg font-black uppercase tracking-wider text-zinc-500">
+                Early Access
+              </h3>
+            </div>
+
+            <BuyWithStripe />
+          </section>
+        </div>
       </div>
     </div>
   );
