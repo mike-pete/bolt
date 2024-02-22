7cb5d95af8a8d8b817d134c264188f5250d8354b
Mike Peterson <mike@lemonshell.com>
2024-02-21 20:58:12 -0700

    update link

--- web/src/app/_components/BuyWithStripe.tsx
+++ web/src/app/_components/BuyWithStripe.tsx
@@ -3,10 +3,10 @@ import { IconArrowRight } from "@tabler/icons-react";
 const BuyWithStripe: React.FC = () => {
   return (
     <a
-      className="group flex items-center rounded-lg bg-sky-400 p-4 text-xl font-bold text-white gap-2 uppercase"
-      href="https://buy.stripe.com/5kA7vYea82iLdKo144"
+      className="group flex items-center gap-2 rounded-lg bg-sky-400 p-4 text-xl font-bold uppercase text-white"
+      href="https://buy.stripe.com/dR617A4zy0aD6hW6op"
     >
-      Buy Now <IconArrowRight size={30} stroke={3}/>
+      Buy Now <IconArrowRight size={30} stroke={3} />
     </a>
   );
 };
