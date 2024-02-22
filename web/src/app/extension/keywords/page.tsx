f501a7d3df71dc4e8606215d853d8a886e346271
Mike Peterson <mike@lemonshell.com>
2024-02-21 22:18:07 -0700

    move stuff around

--- web/src/app/extension/keywords/page.tsx
+++ web/src/app/extension/keywords/page.tsx
@@ -9,27 +9,33 @@ import NewKeywordGroupButton from "./NewKeywordGroupButton";
 const Keywords: React.FC = () => {
   const { data: keywordGroups, isLoading: loadingKeywordGroups } =
     api.keywords.getKeywordGroups.useQuery();
+
+  if (loadingKeywordGroups) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <LoadingSpinner size={30} />
+      </div>
+    );
+  }
+
   return (
-    <div className="flex flex-col flex-nowrap gap-4 items-center p-4">
-      {loadingKeywordGroups && <LoadingSpinner size={30}/>}
+    <div className="flex flex-col flex-nowrap items-center gap-4 p-4">
       {keywordGroups?.map((keywordGroup) => (
         <KeywordGroup keywordGroup={keywordGroup} key={keywordGroup.id} />
       ))}
-      {keywordGroups?.length === 0 && <NoKeywordGroups />}
+      {keywordGroups?.length === 0 && (
+        <div className="flex h-full flex-col items-center justify-center gap-1 rounded-lg pb-4 pt-8 text-center">
+          <h3 className="w-full text-lg font-semibold">
+            What do you want to know?
+          </h3>
+          <p className="pb-2">
+            Add keywords to see what each job offers at a glance.
+          </p>
+        </div>
+      )}
       <NewKeywordGroupButton />
     </div>
   );
 };
-
-const NoKeywordGroups = () => (
-  <div className="flex h-full flex-col items-center justify-center gap-1 rounded-lg pt-8 pb-4 text-center">
-      <h3 className="w-full text-lg font-semibold">
-          What do you want to know?
-      </h3>
-      <p className="pb-2">
-          Add keywords to see what each job offers at a glance.
-      </p>
-  </div>
-)
 
 export default Keywords;
