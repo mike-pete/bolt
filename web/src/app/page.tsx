4f0851f3881c57b7cdf01bc0b0cbd33c96fa2ce5
Mike Peterson <mike@lemonshell.com>
2024-02-21 23:15:20 -0700

    add links to homepage

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { IconBrandChrome, IconBrandDiscordFilled } from "@tabler/icons-react";
 import Image from "next/image";
 import BuyWithStripe from "./_components/BuyWithStripe";
 import NavBar from "./_components/NavBar";
@@ -8,8 +9,8 @@ export default async function Home() {
       <NavBar />
       <div className="flex justify-center">
         <div className="flex max-w-md flex-col p-8">
-          <section className="flex flex-col h-screen">
-            <div className="flex flex-col items-center h-4/6 justify-center">
+          <section className="flex h-screen flex-col">
+            <div className="flex h-4/6 flex-col items-center justify-center">
               <div className="flex items-center gap-4">
                 <Image src="/bolt.svg" alt="Bolt" width={40} height={40} />
                 <h1 className="text-7xl font-semibold">Bolt</h1>
@@ -19,7 +20,25 @@ export default async function Home() {
               </h3>
             </div>
 
-            <BuyWithStripe />
+            <div className="flex flex-col items-start gap-2">
+              <BuyWithStripe />
+              <a
+                className="flex gap-2 rounded-lg bg-zinc-200 p-2 text-lg font-semibold"
+                href="https://chromewebstore.google.com/detail/bolt-apply/iieomcnmomejdefhjfdljckabjldeenb?hl=en&authuser=0"
+                target="_blank"
+              >
+                Chrome Store
+                <IconBrandChrome />
+              </a>
+              <a
+                className="flex gap-2 rounded-lg bg-zinc-200 p-2 text-lg font-semibold"
+                href="https://discord.gg/gBB8uVarFn"
+                target="_blank"
+              >
+                Discord
+                <IconBrandDiscordFilled />
+              </a>
+            </div>
           </section>
         </div>
       </div>
