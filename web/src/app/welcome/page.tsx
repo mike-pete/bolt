0d9e416f30f51c1715b8d36f44069ee672f3674b
Mike Peterson <mike@lemonshell.com>
2024-02-22 20:21:19 -0700

    update welcome page

--- web/src/app/welcome/page.tsx
+++ web/src/app/welcome/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+import { SessionProvider } from "next-auth/react";
+import GoogleAuthButton from "../_components/GoogleAuthButton";
+import { IconArrowDown } from "@tabler/icons-react";
+
 const Welcome = () => {
   return (
-    <div>
-      <h1>Welcome!</h1>
+    <div className="flex h-screen flex-col items-center justify-center gap-6">
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h1 className="text-3xl font-bold text-zinc-700">
+          Thank you for installing Bolt!
+        </h1>
+        <p>Sign in below to get started.</p>
+        <IconArrowDown />
+      </div>
+      <SessionProvider>
+        <GoogleAuthButton />
+      </SessionProvider>
     </div>
   );
 };
