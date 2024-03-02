af0bfbd7d1b4253416082b8797471ec9e3714209
Mike Peterson <mike@lemonshell.com>
2024-03-02 14:09:19 -0700

    group jobs by date saved

--- web/src/app/(web)/dashboard/SavedJobs.tsx
+++ web/src/app/(web)/dashboard/SavedJobs.tsx
@@ -1,30 +1,56 @@
 "use client";
 
+import dayjs from "dayjs";
+import { useMemo } from "react";
 import { api } from "~/trpc/react";
-import { RouterOutputs } from "~/trpc/shared";
+import { type RouterOutputs } from "~/trpc/shared";
 
 type JobPreviewType = RouterOutputs["jobs"]["getJobPreviews"][number];
 
 const SavedJobs: React.FC = () => {
   const { data: savedJobs } = api.jobs.getJobPreviews.useQuery();
 
-  console.log(savedJobs);
+  const jobsByDate = useMemo(() => {
+    const dates: { date: string; jobs: Omit<JobPreviewType, "createdAt">[] }[] =
+      [];
+
+    if (savedJobs) {
+      savedJobs.forEach(({ createdAt, ...job }) => {
+        const date = dayjs(createdAt).format("MMM D, YYYY");
+
+        if (dates.at(-1)?.date === date) {
+          dates.at(-1)?.jobs.push(job);
+          return;
+        } else dates.push({ date, jobs: [job] });
+      });
+    }
+
+    return dates;
+  }, [savedJobs]);
 
   return (
     <div>
       <h1>Saved Jobs</h1>
       <div className="flex flex-col gap-4">
-        {savedJobs?.map((job) => <JobPreview key={job.id} {...job} />)}
+        {jobsByDate?.map((job) => (
+          <div key={job.date}>
+            <h2>{job.date}</h2>
+            <div className="flex flex-col gap-2">
+              {job.jobs.map((job) => (
+                <JobPreview key={job.id} {...job} />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
-const JobPreview: React.FC<JobPreviewType> = ({
+const JobPreview: React.FC<Omit<JobPreviewType, "createdAt">> = ({
   id,
   title,
   company,
-  createdAt,
   url,
   jobId,
 }) => {
@@ -32,7 +58,6 @@ const JobPreview: React.FC<JobPreviewType> = ({
     <div className="rounded-lg border-2 bg-zinc-100 p-2">
       <h2>{title}</h2>
       <p>{company}</p>
-      <p>{createdAt.toString()}</p>
     </div>
   );
 };
