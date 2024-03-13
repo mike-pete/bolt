3484dae2047ac2136b8b2a74ea1d95a781ca0751
Mike Peterson <mike@lemonshell.com>
2024-03-12 20:33:53 -0600

    remove warning

--- web/src/app/(web)/auth/signin/page.tsx
+++ web/src/app/(web)/auth/signin/page.tsx
@@ -1,16 +1,8 @@
-import { IconExclamationCircle } from "@tabler/icons-react";
 import GoogleAuthButton from "~/app/_components/GoogleAuthButton";
 
 const SignIn: React.FC = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-8">
-      <div className="flex max-w-96 gap-2 rounded-lg border bg-zinc-50 p-2 text-red-600">
-        <IconExclamationCircle size={20} />
-        <p className="gap-2 text-sm font-semibold opacity-85">
-          If your account {"hasn't"} been activated you {"won't"} be able to
-          login.
-        </p>
-      </div>
       <GoogleAuthButton />
     </div>
   );
--- web/src/app/(web)/welcome/page.tsx
+++ web/src/app/(web)/welcome/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { IconArrowDown, IconExclamationCircle } from "@tabler/icons-react";
+import { IconArrowDown } from "@tabler/icons-react";
 import { SessionProvider } from "next-auth/react";
 import GoogleAuthButton from "../../_components/GoogleAuthButton";
 
@@ -16,13 +16,6 @@ const Welcome = () => {
       <SessionProvider>
         <GoogleAuthButton />
       </SessionProvider>
-      <div className="flex max-w-96 gap-2 rounded-lg border bg-zinc-50 p-2 text-red-600">
-        <IconExclamationCircle size={20} />
-        <p className="gap-2 text-sm font-semibold opacity-85">
-          If your account {"hasn't"} been activated you {"won't"} be able to
-          login.
-        </p>
-      </div>
     </div>
   );
 };
