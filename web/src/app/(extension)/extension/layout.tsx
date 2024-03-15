2c2fc205397b81f2961deeadf936f7e3ccd570f1
Mike Peterson <mike@lemonshell.com>
2024-03-15 16:03:35 -0600

    update posthog for extension

--- web/src/app/(extension)/extension/layout.tsx
+++ web/src/app/(extension)/extension/layout.tsx
@@ -1,10 +1,22 @@
 "use client";
 
-import { type ReactNode } from "react";
+import { useSession } from "next-auth/react";
+import posthog from "posthog-js";
+import { useEffect, type ReactNode } from "react";
 import AuthScreen from "./AuthScreen";
 import NavBar from "./NavBar";
 
 const ExtensionLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session === null) {
+      posthog.reset();
+    } else if (session?.user.email) {
+      posthog.identify(session.user.email);
+    }
+  }, [session, session?.user.email]);
+
   return (
     <div className="flex h-screen max-h-screen flex-col flex-nowrap overflow-hidden bg-zinc-50">
       <AuthScreen>
