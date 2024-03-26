db672adf242031e3c224a1b6d0dc6cdbf0e8c609
Mike Peterson <mike@lemonshell.com>
2024-03-26 14:15:09 -0600

    search saved jobs

--- web/src/app/(extension)/extension/(standard)/saved/page.tsx
+++ web/src/app/(extension)/extension/(standard)/saved/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+import { IconSearch } from "@tabler/icons-react";
+import { useState } from "react";
 import SavedJobs from "~/app/_components/SavedJobs";
 
 const History: React.FC = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <div className="p-4">
-      <SavedJobs />
+      <div className="relative flex flex-nowrap items-center pb-4">
+        <IconSearch
+          size={20}
+          stroke={3}
+          className="absolute left-3 text-zinc-500"
+        />
+        <input
+          placeholder="Search"
+          className="w-full gap-1 rounded-lg border-2 bg-white p-2 pl-9 font-semibold text-zinc-800"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.currentTarget.value)}
+        />
+      </div>
+      <SavedJobs search={search} />
     </div>
   );
 };
