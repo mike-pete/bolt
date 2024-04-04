b39ce81dbeeb55b65c96c76fadc147bb9f763a28
Mike Peterson <mike@lemonshell.com>
2024-04-04 07:55:07 -0600

    hide extension automatically

--- web/src/app/(extension)/extension/_interactions/handleLocationChanged.ts
+++ web/src/app/(extension)/extension/_interactions/handleLocationChanged.ts
@@ -1,8 +1,21 @@
 import { queryClient } from "~/trpc/react";
+import { pageContexts } from "../(standard)/job/scrapingHooks/pageContexts/pageContext";
+import bi from "./bi";
 import { jobKeys } from "./queryKeys";
 
-const handleLocationChanged = async (_location: string) => {
-  await queryClient.invalidateQueries({ queryKey: jobKeys.all });
+const handleLocationChanged = (location: string) => {
+  void queryClient.invalidateQueries({ queryKey: jobKeys.all });
+  const currentURL: string =
+    (JSON.parse(location) as { href?: string })?.href ?? "";
+
+  for (const { match } of pageContexts) {
+    if (match(currentURL)) {
+      void bi.showIframe();
+      return;
+    }
+  }
+
+  void bi.hideIframe();
 };
 
 export default handleLocationChanged;
