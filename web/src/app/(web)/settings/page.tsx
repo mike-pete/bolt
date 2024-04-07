584d1872d7c6be249e13c4ca57918858b5a0adc0
Mike Peterson <mike@lemonshell.com>
2024-04-07 10:49:37 -0600

    add settings page

---
+++ web/src/app/(web)/settings/page.tsx
@@ -0,0 +1,11 @@
+const Settings = () => {
+  return (
+    <div className="flex flex-grow justify-center p-8">
+      <div className="bg-white border-2 rounded-lg p-8 min-w-[50%]">
+        <h1>Settings</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
