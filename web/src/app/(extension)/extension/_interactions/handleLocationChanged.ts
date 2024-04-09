33389a3f8b8902bfac170e47aa37da589e70af02
Mike Peterson <mike@lemonshell.com>
2024-04-09 08:23:15 -0600

    improve show/hide logic

--- web/src/app/(extension)/extension/_interactions/handleLocationChanged.ts
+++ web/src/app/(extension)/extension/_interactions/handleLocationChanged.ts
@@ -3,19 +3,31 @@ import { pageContexts } from "../(standard)/job/scrapingHooks/pageContexts/pageC
 import bi from "./bi";
 import { jobKeys } from "./queryKeys";
 
+const urlHasJob = (url: string) => {
+  for (const { match } of pageContexts) {
+    if (match(url)) {
+      return true;
+    }
+  }
+  return false;
+};
+
 const handleLocationChanged = (location: string) => {
-  void queryClient.invalidateQueries({ queryKey: jobKeys.all });
+  const prevURL: string = queryClient.getQueryData(jobKeys.currentURL()) ?? "";
   const currentURL: string =
     (JSON.parse(location) as { href?: string })?.href ?? "";
 
-  for (const { match } of pageContexts) {
-    if (match(currentURL)) {
-      void bi.showIframe();
-      return;
-    }
+  void queryClient.invalidateQueries({ queryKey: jobKeys.all });
+
+  if (!urlHasJob(currentURL)) {
+    void bi.hideIframe();
+    return;
   }
 
-  void bi.hideIframe();
+  if (!urlHasJob(prevURL) && urlHasJob(currentURL)) {
+    void bi.showIframe();
+    return;
+  }
 };
 
 export default handleLocationChanged;
