ad599c09b0d1d47ecc7d6217ac131e0e545631e0
Mike Peterson <mike@lemonshell.com>
2024-04-19 12:55:56 -0600

    wip

--- web/src/app/(web)/dashboard/JobNotesModal.tsx
+++ web/src/app/(web)/dashboard/JobNotesModal.tsx
@@ -45,16 +45,16 @@ const JobNotesModal: React.FC<{
       onClose={onClose}
       className="fixed inset-0 z-50 flex items-center justify-center bg-zinc-400/40 p-4 backdrop-blur-sm"
     >
-      <Dialog.Panel className="max-h-[95vh] w-[50ch] max-w-full overflow-hidden rounded-lg bg-zinc-50 shadow-lg">
+      <Dialog.Panel className="max-h-[95vh] w-[45ch] max-w-full overflow-hidden rounded-lg bg-zinc-50 shadow-lg">
         <div className="relative flex max-h-[inherit] min-h-[45ch] max-w-full flex-col overflow-y-auto overflow-x-hidden">
-          <div className="flex flex-grow flex-col">
-            <div className="flex flex-col w-full items-end gap-4 z-10 border-b-2 p-4 bg-white">
+          <div className="flex flex-grow flex-col p-4">
+            <div className="z-10 flex w-full flex-col items-end rounded-lg border-2 bg-white p-4">
               <div className="max-h-[30vh] w-full flex-grow overflow-hidden rounded-lg bg-white">
                 <TextArea
                   value={val}
                   setValue={setVal}
                   placeholder="new note..."
-                  className="max-h-[inherit] w-full resize-none overflow-y-auto rounded-lg p-4 outline-none"
+                  className="max-h-[inherit] w-full resize-none overflow-y-auto rounded-lg outline-none"
                 />
               </div>
               <button
@@ -65,18 +65,15 @@ const JobNotesModal: React.FC<{
               </button>
             </div>
 
-            <div className="flex max-h-[50vh] flex-grow flex-col gap-2 overflow-auto rounded-b-lg p-4">
+            <div className="flex max-h-[50vh] flex-grow flex-col gap-2 overflow-auto rounded-b-lg py-4">
               {!notes?.length && (
                 <p className="flex justify-center rounded-lg p-4 text-sm font-semibold uppercase text-zinc-400">
                   No saved notes
                 </p>
               )}
               {notes?.map(({ id, note, createdAt }) => (
-                <div
-                  className="flex flex-col gap-1 rounded-lg p-4"
-                  key={id}
-                >
-                  <p className="text-xs text-zinc-400 font-semibold">
+                <div className="flex flex-col gap-1 rounded-lg p-4" key={id}>
+                  <p className="text-xs font-semibold text-zinc-400">
                     {dayjs(createdAt).format("MMMM DD, YYYY")}
                   </p>
                   <TextArea
