ff35c9fb61ca477652233e93f7e4482f7bd9dec4
Mike Peterson <mike@lemonshell.com>
2024-04-24 13:03:27 -0600

    enable note deletion

--- web/src/app/(web)/dashboard/JobNotesModal.tsx
+++ web/src/app/(web)/dashboard/JobNotesModal.tsx
@@ -1,7 +1,8 @@
 import { Status } from ".prisma/client";
 import { Dialog } from "@headlessui/react";
-import { IconX } from "@tabler/icons-react";
+import { IconTrash, IconX } from "@tabler/icons-react";
 import dayjs from "dayjs";
+import posthog from "posthog-js";
 import { useRef, useState } from "react";
 import { type JobDetails } from "~/app/_components/JobCard/JobCard";
 import TextArea from "~/app/_components/TextArea/TextArea";
@@ -13,14 +14,51 @@ const JobNotesModal: React.FC<{
   isLoadingJobDetails: boolean;
   jobDetails?: JobDetails;
 }> = ({ open, onClose, jobDetails }) => {
-  const [val, setVal] = useState("");
+  const [currentNote, setCurrentNote] = useState("");
 
   const utils = api.useUtils();
 
   const saveJob = api.jobs.saveJob.useMutation({
     onSuccess: () => {
-      setVal("");
+      setCurrentNote("");
+      posthog.capture("job_note_created", {
+        jobId: jobDetails?.jobId,
+        note: currentNote,
+      });
       // TODO: optimistic update job/jobs
+    },
+    onSettled: () => {
+      void utils.jobs.getJob.invalidate();
+      void utils.jobs.getJobs.invalidate();
+    },
+  });
+
+  const deleteNote = api.jobs.deleteNote.useMutation({
+    onMutate: (noteId) => {
+      utils.jobs.getJob.setData(jobDetails?.jobId ?? "", (prev) => {
+        if (!prev) return prev;
+        return {
+          ...prev,
+          notes: prev.notes?.filter((note) => note.id !== noteId),
+        };
+      });
+      utils.jobs.getJobs.setData(undefined, (prev) => {
+        if (!prev) return prev;
+        return prev.map((job) => {
+          if (job.jobId === jobDetails?.jobId) {
+            return {
+              ...job,
+              notes: job.notes?.filter((note) => note.id !== noteId),
+            };
+          }
+          return job;
+        });
+      });
+    },
+    onSuccess: () => {
+      posthog.capture("job_note_deleted", {
+        jobId: jobDetails?.jobId,
+      });
     },
     onSettled: () => {
       void utils.jobs.getJob.invalidate();
@@ -36,7 +74,7 @@ const JobNotesModal: React.FC<{
       ...jobDetails,
       status: jobDetails?.status ?? Status.Saved,
       compensation: jobDetails?.compensation ?? undefined,
-      note: val.trim(),
+      note: currentNote.trim(),
     });
   };
 
@@ -50,7 +88,7 @@ const JobNotesModal: React.FC<{
       className="fixed inset-0 z-40 flex items-center justify-center bg-zinc-400/40 p-4 backdrop-blur-sm"
       initialFocus={initialFocusRef}
     >
-      <Dialog.Panel className="max-h-[95vh] w-[45ch] max-w-full overflow-hidden rounded-lg bg-zinc-50 shadow-lg">
+      <Dialog.Panel className="w-[45ch] max-w-full overflow-hidden rounded-lg bg-zinc-50 shadow-lg">
         <div className="flex w-full justify-end border-b-2 bg-white p-1">
           <button className="z-50 rounded-lg" onClick={onClose}>
             <IconX
@@ -59,51 +97,62 @@ const JobNotesModal: React.FC<{
             />
           </button>
         </div>
-        <div className="relative flex max-h-[inherit] min-h-[60ch] max-w-full flex-col overflow-y-auto overflow-x-hidden">
-          <div className="flex flex-grow flex-col p-4 pb-0 gap-4">
 
-              <div className="flex w-full flex-col items-end rounded-lg border-2 bg-white p-4">
-                <div className="max-h-[30vh] w-full flex-grow overflow-hidden rounded-lg bg-white">
-                  <TextArea
-                    value={val}
-                    setValue={setVal}
-                    placeholder="new note..."
-                    className="max-h-[inherit] w-full resize-none overflow-y-auto rounded-lg outline-none"
-                    ref={initialFocusRef}
-                  />
-                </div>
-                <button
-                  className="rounded-lg bg-sky-400 px-2 py-1 text-sm font-semibold uppercase text-white disabled:bg-zinc-300"
-                  onClick={createNote}
-                  disabled={saveJob.isLoading || val.trim() === ''}
-                >
-                  {saveJob.isLoading ? "Saving..." : "Save"}
-                </button>
+        <div className="relative max-h-[90vh] min-h-[60ch] max-w-full overflow-auto">
+          <div className="p-4 pb-0">
+            <div className="flex w-full flex-col items-end rounded-lg border-2 bg-white p-4">
+              <div className="max-h-[30vh] w-full flex-grow rounded-lg bg-white">
+                <TextArea
+                  value={currentNote}
+                  setValue={setCurrentNote}
+                  placeholder="new note..."
+                  className="max-h-[inherit] w-full resize-none overflow-y-auto rounded-lg outline-none"
+                  ref={initialFocusRef}
+                />
               </div>
+              <button
+                className="rounded-lg bg-sky-400 px-2 py-1 text-sm font-semibold uppercase text-white disabled:bg-zinc-300"
+                onClick={createNote}
+                disabled={saveJob.isLoading || currentNote.trim() === ""}
+              >
+                {saveJob.isLoading ? "Saving..." : "Save"}
+              </button>
+            </div>
 
-              <div className="flex flex-grow flex-col gap-2 overflow-auto rounded-b-lg">
-                {!notes?.length && (
-                  <p className="flex justify-center rounded-lg p-4 text-sm font-semibold uppercase text-zinc-400">
-                    No saved notes
-                  </p>
-                )}
-                {notes?.map(({ id, note, createdAt }) => (
-                  <div className="flex flex-col gap-1 rounded-lg p-4" key={id}>
-                    <p className="text-xs font-semibold text-zinc-400">
+            <div className="flex flex-grow flex-col gap-2 rounded-b-lg py-4">
+              {!notes?.length && (
+                <p className="flex justify-center rounded-lg p-4 text-sm font-semibold uppercase text-zinc-400">
+                  No saved notes
+                </p>
+              )}
+              {notes?.map(({ id, note, createdAt }) => (
+                <div
+                  className="group flex flex-col gap-1 rounded-lg p-4 duration-150 hover:bg-zinc-100"
+                  key={id}
+                >
+                  <div className="flex">
+                    <p className="flex-grow text-xs font-semibold text-zinc-400">
                       {dayjs(createdAt).format("MMMM DD, YYYY")}
                     </p>
-                    <TextArea
-                      value={note}
-                      setValue={() => {
-                        return;
-                      }}
-                      className="w-full resize-none bg-transparent"
-                      disabled
-                    />
+                    <button
+                      className="text-transparent duration-150  group-hover:text-zinc-400"
+                      onClick={() => deleteNote.mutate(id)}
+                    >
+                      <IconTrash size={16} className="hover:text-red-500" />
+                    </button>
                   </div>
-                ))}
-              </div>
+                  <TextArea
+                    value={note}
+                    setValue={() => {
+                      return;
+                    }}
+                    className="w-full resize-none bg-transparent"
+                    disabled
+                  />
+                </div>
+              ))}
             </div>
+          </div>
         </div>
       </Dialog.Panel>
     </Dialog>
