23e21334cf2ba44f46d1bd8b6afa47cac136f1b6
Mike Peterson <mike@askluca.com>
2024-11-24 17:29:36 -0800

    migrate to neon

--- web/src/server/db.ts
+++ web/src/server/db.ts
@@ -1,21 +1,24 @@
-import { Client } from "@planetscale/database";
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-
+import ws from "ws";
 import { env } from "~/env.js";
+
+const connectionString = env.DATABASE_URL;
+neonConfig.webSocketConstructor = ws;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
-
-const client = new Client({ url: env.DATABASE_URL });
 
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(client),
+    adapter,
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
